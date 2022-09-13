a8282e5fe60146a39395569b50ee5ea60fb72399
JustAung <112411778+JustAung@users.noreply.github.com>
2022-09-13 17:24:57 -0400

    Add files via upload

---
+++ MyMondaySchedule.java
@@ -0,0 +1,19 @@
+public class MyMondaySchedule{
+
+	public static void main(String[] args)
+	{
+		System.out.println("On Monday I wake up at " + 7 + " AM " 
+		+ "and I brush my teeth.");
+		System.out.println("I wash my face and eat breakfast around " + 8 + " AM. ");
+		System.out.println("I get all my things ready for school by " + 9 + " AM.");
+		System.out.println("I drive to school at " + 9 + " AM " + ", an hour before class.");
+		System.out.println("I arrive and go to class at " + 10 + " AM.");
+		System.out.println("I attend all my classes and then head home at " + 1 + " PM. .");
+		System.out.println("I get home around " + 2 + " PM " + "and eat around " + 3 + " PM.");
+		System.out.println("I spend time on my homework after " + 6 + " PM.");
+		System.out.println("I go to shower around " + 10 + " PM " + "and go to sleep around " + 1 + " AM.");
+	
+	}
+
+
+}
